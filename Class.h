f0d524502e7e4d7a7d3e05fd0f4d90bacd96c295
SophieRuetschi <92867229+SophieRuetschi@users.noreply.github.com>
2022-12-05 21:42:44 -0500

    Add files via upload

---
+++ Class.h
@@ -0,0 +1,185 @@
+//Allison Vollmer, Brooke Nelson, Sophie Ruetschi
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include <sstream>
+#include <map>
+using namespace std;
+//-------------------------------------AREA CLASS-----------------------------------
+class Area{
+private:
+  int objectID;
+  string stateFIPS;
+  string countyFIPS;
+  double natWalkIndex;
+
+public:
+  Area();
+  Area(int object, string state, string county, double index);
+  double getIndex();
+  string getState();
+  string getCounty();
+  int getObjID();
+};
+
+//default Area constructor- initalize everything to 0
+Area::Area(){
+  objectID = 0;
+  stateFIPS = '0';
+  countyFIPS = '0';
+  natWalkIndex = 0;
+};
+
+//Area constructor- initalize everything to respective arguments
+Area::Area(int object, string state, string county, double index){
+  objectID = object;
+  stateFIPS = state;
+  countyFIPS = county;
+  natWalkIndex = index;
+};
+
+//index accessor
+double Area::getIndex(){
+  return natWalkIndex;
+}
+
+//state FIPS code accessor
+string Area::getState() {
+  return stateFIPS;
+}
+
+//county FIPS code accessor
+string Area::getCounty() {
+  return countyFIPS;
+}
+
+//object ID accessor
+int Area::getObjID() {
+    return objectID;
+}
+
+//-------------------------------------FIPS CLASSES-----------------------------------
+//class to act as County FIPS dictionary 
+class CountyFIPS{
+private:
+    map<string, string> FIPSData;
+
+public:
+    CountyFIPS();
+    string getCountyName(string num);
+    string getCountyNumber(string name);
+};
+
+//constructor
+// used Lab 4 from Programming Fundamentals 2 as reference for reading from csv file
+CountyFIPS::CountyFIPS(){
+  
+  // open file
+  fstream inFile;
+  inFile.open("CityCounty_FIPS.txt");
+
+  string line;
+  string num;
+  string name;
+  string space;
+
+  while(getline(inFile, line)) {
+    stringstream s(line);
+
+    //read past whitespace
+    getline(s, space, ' ');
+    getline(s, space, ' ');
+    getline(s, space, ' ');
+    getline(s, space, ' ');
+
+    //get the county FIPS code
+    getline(s, num, ' ');
+
+    //read past the white space
+    getline(s, space, ' ');
+    getline(s, space, ' ');
+    getline(s, space, ' ');
+    getline(s, space, ' ');
+    getline(s, space, ' ');
+    getline(s, space, ' ');
+    getline(s, space, ' ');
+
+    //get the name of the county
+    getline(s, name);
+
+    //push new infromation into the map
+    FIPSData[num] = name;
+  }
+}
+
+//return the string that matches the FIPS number
+string CountyFIPS::getCountyName(string num){
+    if(FIPSData.find(num) == FIPSData.end())
+        return num;
+    return FIPSData[num];
+}
+
+//returns the FIPS number associated with the string, -1 if no match found
+string CountyFIPS::getCountyNumber(string name){
+
+  for(auto it = FIPSData.begin(); it != FIPSData.end(); it++){
+    if(it->second == name)
+      return it->first;
+  }
+  return "";
+}
+
+//class to act as County FIPS dictionary 
+class StateFIPS{
+private:
+    map<string, string> FIPSData;
+
+public:
+    StateFIPS();
+    string getStateName(string num);
+    string getStateNumber(string name);
+};
+
+// used Lab 4 from Programming Fundamentals 2 as reference for reading from csv file
+StateFIPS::StateFIPS(){
+
+  //open the file
+  fstream inFile;
+  inFile.open("StateFIPS.txt");
+
+  string line;
+  string num;
+  string abv;
+  string state;
+
+  //read past the header
+  getline(inFile, line);
+
+  //loop through whole file
+  while (getline(inFile, line)) {
+    stringstream s(line);
+
+    //seperate out the relevant infromation
+    getline(s, num, '|');
+    getline(s, abv, '|');
+    getline(s, state, '|');
+
+    //push back new info into the map
+    FIPSData[num] = state;
+  }
+}
+
+//function to return the string name of a state given the FIPS code
+string StateFIPS::getStateName(string FIPS) {
+  return FIPSData[FIPS];
+}
+
+//function to return the FIPS code of a state given the string name
+string StateFIPS::getStateNumber(string name){
+
+  for(auto it = FIPSData.begin(); it != FIPSData.end(); it++){
+    if(it->second == name)
+      return it->first;
+  }
+  return "";
+}
